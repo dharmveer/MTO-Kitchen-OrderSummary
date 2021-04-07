a20c19e244c5e99c754e7d669a88e35461d264ef
Dharmveer kumar <dharmveer.apiit@gmail.com>
2021-04-07 12:24:05 +0530

    first-commit

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { randomBytes } = require('crypto');
 const cors = require('cors');
-//const axios = require('axios');
-const JsBarcode = require('jsbarcode');
 const path = require('path');
 const app = express();
 app.use(bodyParser.json());
@@ -11,10 +8,10 @@ app.use(cors());
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.get('/generateBarcode/loyaltyNum/:loyaltynum', (req, res) => {
+app.get('/barcode/loyaltyNum/:loyaltynum', (req, res) => {
     res.render('orderInfo', {loyalty: req.params.loyaltynum});
 });
 
-app.listen(4000, () => {
-  console.log('Listening on 4000');
+app.listen(80, () => {
+  console.log('Listening on 80');
 });
